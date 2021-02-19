c23ef9be3a7c9d1691e03ce308d643e739f30cd4
IgorBashkov <bashkov.torvm36@gmail.com>
2021-02-19 23:09:04 +0300

    2

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 from flask import Flask
-
+from frh_cls import fahrenheit_from
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello!</h1>'
+    celsius = request.args.get('celsius', '')
+    if celsius:
+        fahrenheit = fahrenheit_from(celsius)
+    else:
+        fahrenheit = ''
+
+    returned_text = f"""<h1>Hello!</h1>
+    <div>You should input float value if degrees in celsius and press 'Conver' button.<br>
+    The result will appear lower.</div>
+    <div>
+        <form action="" method="get">
+            Celsius temperature: <input type="text" name="celsius">
+            <input type="submit" value="Convert to Fahrenheit">
+        </form>
+    </div>
+    <h3>Fahrenheit:</h3>
+    <h3>{fahrenheit}</h3>"""
+    return returned_text
 
 
 if __name__ == '__main__':
